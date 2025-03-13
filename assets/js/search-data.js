6f2625fdb432bda7a6d7993e2bbdce7ff9bf5825
gwangheekim <63139661+gwangheekim@users.noreply.github.com>
2025-03-13 10:16:28 +0000

    Deploying to gh-pages from @ Yonseiserver/Yonseiserver.github.io@970c74ddc49d20527219d0e0b17d5253d4751b42 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,17 +41,6 @@ ninja.data = [{
           window.location.href = "/blog/2025/tf/";
         
       },
-    },{id: "post-ssh-접속-및-python-실행하기",
-      
-        title: "SSH 접속 및 Python 실행하기",
-      
-      description: "SSH 접속 방법 및 HPC에서 Python 실행 방법",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2025/ssh-python/";
-        
-      },
     },{id: "post-r-실행하기",
       
         title: "R 실행하기",
@@ -61,6 +50,17 @@ ninja.data = [{
       handler: () => {
         
           window.location.href = "/blog/2025/r/";
+        
+      },
+    },{id: "post-ssh-접속-및-python-실행하기",
+      
+        title: "SSH 접속 및 Python 실행하기",
+      
+      description: "SSH 접속 방법 및 HPC에서 Python 실행 방법",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/ssh-python/";
         
       },
     },{id: "post-introduction",
@@ -74,48 +74,10 @@ ninja.data = [{
           window.location.href = "/blog/2025/introduction/";
         
       },
-    },{id: "post-a-post-with-image-galleries",
-      
-        title: "a post with image galleries",
-      
-      description: "this is what included image galleries could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/photo-gallery/";
-        
-      },
-    },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-      
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-        
-      },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-        
-      },
     },{id: "news-we-are-excited-to-announce-the-launch-of-our-brand-new-high-performance-computing-hpc-cluster-server-this-new-server-will-provide-significantly-improved-computing-power-for-our-students-and-researchers",
           title: 'We are excited to announce the launch of our brand new High-Performance Computing...',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
